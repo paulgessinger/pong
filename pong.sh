d44751d19481451468ed17ae9d84a26c0aa4d0cb
Paul Gessinger <hello@paulgessinger.com>
2012-12-31 15:15:17 +0100

    script change.

--- pong.sh
+++ pong.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+current_dir=$(pwd)
+echo $current_dir
+
 function stop {
    forever  -o ./out.log -e ./err.log -m 1 stop server.js 
 }
 
 
 function start {
-   forever  -o ./out.log -e ./err.log -m 1 start server.js 
+   forever -p $BASEDIR -a -l ./pong.log -o ./out.log -e ./err.log -m 1 start server.js
 }
 
 function restart {
